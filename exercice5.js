f409fbc66ef089ea092c384180256f24df35409d
Pierreabc <99174513+Pierreabc@users.noreply.github.com>
2022-12-04 21:30:24 +0100

    Update exercice5.js

--- exercice5.js
+++ exercice5.js
@@ -1,8 +1,19 @@
-function array(arr){
-    let result = arr.sort(function (a, b) {  return a - b;  })
-    console.log(result)
-    return result
+function superieur(num) {
+    
+  for (let i = 0; i < num.length; i++) {
+      for (let x = i + 1; x < num.length; x++) {
+          if (num[i] > num[x]) {
+              
+              let temp = num[i];
+              num[i] = num[x];
+              num[x] = temp;
+              
+          }
+      }
+  }
+  return num;
 }
 
 
-array([5,88,76,8,736,10])
+
+console.log(superieur([78, 57, 39, 2, 21]));
