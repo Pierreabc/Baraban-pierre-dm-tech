6c4d6007012cb2386df21c20dcaced0a898bb8b8
Pierreabc <99174513+Pierreabc@users.noreply.github.com>
2022-12-04 21:28:38 +0100

    Update exercice3.js

--- exercice3.js
+++ exercice3.js
@@ -1,20 +1,5 @@
-const prompt = require("prompt-sync")();
-
-function numberaddt() {
-
-    let inputNumber = prompt("Choisir votre nombre : ");
-    let inputToInt = parseInt(inputNumber)
-
-    if(isNaN(inputToInt)){
-        console.log("c'est autre chose qu'un chiffre")
-        numberaddt()
-    }else{
-        let maxNumber = inputToInt + 10
-        for(let i = 0; inputToInt != maxNumber; i++){
-            inputToInt++;
-            console.log(inputToInt);
-        }
-    }
-}
- 
-numberaddt();
+let start = 17;
+for (let cpt = start +1; cpt < start +11; cpt++) {
+    
+    console.log(cpt);
+  }
