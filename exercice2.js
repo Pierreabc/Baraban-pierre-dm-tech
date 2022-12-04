c52823ff0453909a09ce0dc0ec0203b5f17ac64b
Pierreabc <99174513+Pierreabc@users.noreply.github.com>
2022-12-04 21:26:41 +0100

    Update exercice2.js

--- exercice2.js
+++ exercice2.js
@@ -1,6 +1,14 @@
-function detecteur(array) { 
-    let max = Math.max.apply(Math, array)
-    console.log(max)
-    }
-    
-    detecteur([36,267,29])
+function filtre(a, b ,c){
+  
+  if (a > b) {
+    console.log(a)
+  }
+  else if (b > c) {
+    console.log(b)
+  }
+  else{
+    console.log(c)
+  };
+}
+
+console.log(filtre(18,47,2));
